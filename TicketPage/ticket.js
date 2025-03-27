fa356db64908bc7eda381ffb4c10e202ceab30ff
Danilo Santana <108311986+DSantana04@users.noreply.github.com>
2025-03-27 11:20:04 -0300

    Updating classes and id names
    Just changing some names in the archives for better code understanding

--- TicketPage/ticket.js
+++ TicketPage/ticket.js
@@ -1,25 +1,20 @@
 let name = JSON.parse(localStorage.getItem('userData')).name;
 
-const nome = document.getElementById('nome-titulo').textContent = name;
-const email = document.getElementById('email-titulo').textContent = JSON.parse(localStorage.getItem('userData')).email;
+const nome = document.getElementById('nome-info').textContent = name;
+const email = document.getElementById('email-info').textContent = JSON.parse(localStorage.getItem('userData')).email;
 
-const avatar = document.getElementById('avatar-ticket');
-avatar.src = localStorage.getItem('image'); // Retrieve avatar URL
+const avatar = document.getElementById('avatar-output');
+avatar.src = localStorage.getItem('image');
 
-const githubUsername = JSON.parse(localStorage.getItem('userData')).github; // Retrieve GitHub username
-const github = document.getElementById('github-ticket');
-github.textContent = githubUsername; // Set GitHub username
+const githubUsername = JSON.parse(localStorage.getItem('userData')).github;
+const github = document.getElementById('github-output');
+github.textContent = githubUsername;
 
 const icon = document.createElement('img');
-icon.src = '../Images/icon-github.svg'; // Corrected path for GitHub icon
-icon.width = '20';
-icon.height = '20';
-github.appendChild(icon); // Append icon next to GitHub username
+icon.src = '../Images/icon-github.svg';
+icon.width = '30';
+icon.height = '30';
+github.appendChild(icon); //Adicionar icone github junto ao username
 
 
-const nome_ticket = document.getElementById('name-ticket').innerHTML = name;
-if (localStorage.getItem('ticket') == null || localStorage.getItem('ticket') == undefined) {
-    const random_ticket_number = Math.floor(Math.random() * 1000000);
-    localStorage.setItem('ticket', random_ticket_number);
-}
-let ticket_number = document.getElementById('ticket-number').innerHTML = "#" + localStorage.getItem('ticket');
+const nome_ticket = document.getElementById('nome-output').innerHTML = name;
